ed6e1be622628150a49498c7970407cba0377266
acc22jl <jlee145@sheffield.ac.uk>
2023-11-25 18:05:32 +0000

    adding inventory class

---
+++ Inventory.java
@@ -0,0 +1,15 @@
+public class Inventory {
+    private int quantity;
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void Inventory(int quantity) {
+        this.quantity = quantity;
+    }
+}
