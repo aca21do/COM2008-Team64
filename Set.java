7227a97ce1bcae0a252aa5214047163bffbc8489
acc22jl <jlee145@sheffield.ac.uk>
2023-11-25 17:55:22 +0000

    adding set class

---
+++ Set.java
@@ -0,0 +1,9 @@
+public class Set extends Pack{
+    public void Set(String newBrandName, String newProductName,
+                     String newProductCode, float newRetailPrice) {
+        this.brandName = newBrandName;
+        this.productName = newProductName;
+        this.productCode = newProductCode;
+        this.retailPrice = newRetailPrice;
+    }
+}
