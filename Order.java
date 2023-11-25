46d9f0d46e7582836c90b312b483a94a6a99270e
acc22jl <jlee145@sheffield.ac.uk>
2023-11-25 18:58:22 +0000

    adding gets and sets to order

--- Order.java
+++ Order.java
@@ -2,6 +2,28 @@ public class Order {
     protected String orderNumber;
     protected String orderDate;
     protected String orderStatus;
-    protected String deriveTotalCost;
-    
+
+    public String getOrderNumber() {
+        return this.orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getOrderDate() {
+        return this.orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 }
