19f2dde5652b1c6576a434b9530cf0fa6be471d7
acc22jl <jlee145@sheffield.ac.uk>
2023-11-30 17:10:24 +0000

    Adding PendingOrder class

---
+++ PendingOrder.java
@@ -0,0 +1,5 @@
+public class PendingOrder extends Order{
+    public PendingOrder(String orderNumber, String orderDate, String orderStatus) {
+        super(orderNumber, orderDate, orderStatus);
+    }
+}
