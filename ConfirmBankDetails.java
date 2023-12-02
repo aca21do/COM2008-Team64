6856bd97354e695ea65dee33d94919b832ea203a
Peter Morgan <pmorgan1@sheffield.ac.uk>
2023-12-02 01:11:29 +0000

    Add card number to confirm bank details

--- ConfirmBankDetails.java
+++ ConfirmBankDetails.java
@@ -16,13 +16,28 @@ public class ConfirmBankDetails extends JFrame {
         // panel setup
         setContentPane(confirmBankDetailsPanel);
         setTitle("Confirm Bank Details");
-        setSize(400, 350);
+        setSize(400, 300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
         // make staff buttons invisible by default
         staffViewButton.setVisible(false);
         managerViewButton.setVisible(false);
+
+        String cardNo = String.valueOf(CurrentUser.getCurrentUser().hasPayment.getPaymentMethod().getCardNumber());
+        String stars = "**** **** **** ";
+        int length = cardNo.length();
+        if (length < 4) {
+            for (int i = 0; i < 4-length; i++) {
+                stars += "*";
+            }
+            stars += cardNo;
+        }
+        else {
+            stars += cardNo.substring(cardNo.length() - 4);
+        }
+        cardNumberLabel.setText(stars);
+
 
         if (CurrentUser.getCurrentUser().getIsStaff()) {
             staffViewButton.setVisible(true);
@@ -50,19 +65,22 @@ public class ConfirmBankDetails extends JFrame {
         accountButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                new AccountDetails(connection).setVisible(true);
+                setVisible(false);
             }
         });
         staffViewButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                new CatalogueStaff(connection).setVisible(true);
+                setVisible(false);
             }
         });
         managerViewButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                new ViewUsers(connection).setVisible(true);
+                setVisible(false);
             }
         });
     }
